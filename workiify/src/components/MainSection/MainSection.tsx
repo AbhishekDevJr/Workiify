ce4293b53928a73a4ef819bd625c914d77782b1c
abhishek <abhishek@finestardiamonds.com>
2023-10-10 11:23:54 +0530

    Added Notes and Antd Modal

--- workiify/src/components/MainSection/MainSection.tsx
+++ workiify/src/components/MainSection/MainSection.tsx
@@ -3,11 +3,11 @@ import './mainsection.scss';
 import { useSelector } from 'react-redux';
 
 function MainSection() {
-  const mainRender = useSelector((state: any) => state);
-  console.log('state--->', mainRender.mainRender);
+  const mainRender = useSelector((state: any) => state.mainRender);
+  console.log('state--->', mainRender);
   return (
     <div className='container-main-section'>
-      {mainRender.mainRender.byToday ? (<div>Main Section By Today</div>) : (<div>Main Section By Week</div>)}
+      {mainRender.byToday ? (<div>Main Section By Today</div>) : (<div>Main Section By Week</div>)}
     </div>
   )
 }
--- workiify/src/components/Sidebar/Sidebar.tsx
+++ workiify/src/components/Sidebar/Sidebar.tsx
@@ -1,22 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './sidebar.scss';
 import { today, week } from '../../Features/mainRenderSlice';
 import { useDispatch } from 'react-redux';
+import { Button, Modal } from 'antd';
+
+type dataDummy = {
+    name: string,
+    desc: string
+};
+
+const dummyProjects: dataDummy[] = [
+    {
+        name: 'Project 1',
+        desc: ''
+    },
+    {
+        name: 'Project 2',
+        desc: ''
+    },
+    {
+        name: 'Project 3',
+        desc: ''
+    }
+];
+
+const dummyNotes: dataDummy[] = [
+    {
+        name: 'Note 1',
+        desc: ''
+    },
+    {
+        name: 'Note 2',
+        desc: ''
+    },
+    {
+        name: 'Note 3',
+        desc: ''
+    }
+];
 
 function Sidebar() {
     const dispatch = useDispatch();
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const handleSidebarTop = (e: React.MouseEvent<HTMLLIElement>): void => {
         console.log(e.currentTarget.textContent);
         e.currentTarget.textContent === 'Today' ? (dispatch(today())) : (dispatch(week()));
     }
 
+    const handleModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleModalClose = () => {
+        setIsModalOpen(false);
+    }
+
     return (
         <div className='container-sidebar'>
             <div className='top-sidebar'>
-                <strong>
-                    <p>Home</p>
-                </strong>
+                <p>Home</p>
                 <ul>
                     <li onClick={handleSidebarTop}>Today</li>
                     <li onClick={handleSidebarTop}>Week</li>
@@ -24,15 +67,29 @@ function Sidebar() {
             </div>
 
             <div className='bottom-sidebar'>
-                <strong>
-                    <p>Projects</p>
-                </strong>
+                <p>Projects</p>
                 <ul>
-                    <li>Example Project</li>
-                    <li>Example Project</li>
-                    <li>Example Project</li>
-                    <li>Example Project</li>
+                    {dummyProjects.map((item) => <li>{item.name}</li>)}
                 </ul>
+            </div>
+
+            <div className='bottom-notes-container'>
+                <p>Notes</p>
+                <ul>
+                    {dummyNotes.map((item) => <li>{item.name}</li>)}
+                </ul>
+            </div>
+
+            <div className='container-addBtn'>
+                <Button type="primary" onClick={handleModal}>
+                    +
+                </Button>
+
+                <Modal title="Basic Modal" open={isModalOpen} onCancel={handleModalClose}>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                </Modal>
             </div>
         </div>
     )
